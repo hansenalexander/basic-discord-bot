725d9646dbeca2aa1ed4425d964a68beb143bee2
Alexander Hansen <alexahans1@gmail.com>
2020-11-30 22:18:22 +0100

    Initial commit

---
+++ basic_bot.py
@@ -0,0 +1,77 @@
+import random
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord Server!'
+    )
+
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the  :100:  emoji',
+        'Bingpot!',
+        ('Cool. Cool cool cool cool cool cool cool, '
+         'no doubt no doubt no doubt no doubt.'),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [str(random.choice(range(1, number_of_sides + 1)))
+            for _ in range(number_of_dice)
+            ]
+    await ctx.send(','.join(dice))
+
+
+@bot.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name.lower())
+    print(guild)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+# client.run(TOKEN)
+bot.run(TOKEN)
